45f4e935dd8a6bddd25017e52e78350666a73e6a
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-10-28 23:39:14 +0100

    linking to databse

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -4,7 +4,7 @@ import {
   useSessionContext,
   useUser as useSupaUser,
 } from "@supabase/auth-helpers-react";
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 type UserContextType = {
   accessToken: string | null;
@@ -30,4 +30,32 @@ export const MyUserContextProvider = (props: Props) => {
   } = useSessionContext();
 
   const user = useSupaUser();
+  const accessToken = session?.access_token ?? null;
+  const [isLoadingData, setIsLoadingData] = useState(false);
+  const [userDetails, setUserDetails] = useState<UserDetials | null>(null);
+  const [subscription, setSubscription] = useState<Subscription | null>(null);
+
+  const getUserDetails = () => supabase.from("users").select("*").single();
+  const getSubscription = () =>
+    supabase
+      .from("subscription")
+      .select("*, prices(*, products(*))")
+      .in("status", ["trialing", "active"])
+      .single();
+
+  useEffect(() => {
+    if (user && !isLoadingData && !userDetails && !subscription) {
+      setIsLoadingData(true);
+
+      Promise.allSettled([getUserDetails(), getSubscription()]).then(
+        (results) => {
+          const userDetailsPromise = results[0];
+          const subscriptionPromise = results[1];
+          if (userDetailsPromise.status === "fulfilled") {
+            setUserDetails(userDetailsPromise.value.data as UserDetials);
+          }
+        }
+      );
+    }
+  }, []);
 };
