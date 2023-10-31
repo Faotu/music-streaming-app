3e24109001478cf0f9e91beff0121fb567bce66c
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-10-31 16:01:34 +0100

    Upload modal in action

---
+++ components/Input.tsx
@@ -0,0 +1,12 @@
+import { forwardRef } from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, disabled, ...props }, ref) => {
+    return <input />;
+  }
+);
+
+Input.displayName = "Input";
+export default Input;
--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,24 +1,49 @@
 "use client";
-import React from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import React, { useState } from "react";
 import Modal from "./Modal";
 import useUploadModal from "@/hooks/useUploadModal";
+import Input from "./Input";
 
 const UploadModal = () => {
+  const [isLoading, setIsloading] = useState();
   const uploadModal = useUploadModal();
+
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      author: "",
+      title: "",
+      song: null,
+      image: null,
+    },
+  });
+
   const onChange = (open: boolean) => {
     if (!open) {
+      reset();
       uploadModal.onClose();
     }
   };
 
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    // This function will upload a song or file to supabase
+  };
+
   return (
     <Modal
-      title="Upload modal title"
-      description="Upload modal description"
+      title="Add a song"
+      description="Upload an mp3 file"
       isOpen={uploadModal.isOpen}
-      onChange={() => {}}
+      onChange={onChange}
     >
-      Upload Content
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          id="title"
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder="Song title"
+        />
+      </form>
     </Modal>
   );
 };
