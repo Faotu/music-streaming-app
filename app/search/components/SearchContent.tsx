d83f261f6395e8614e0c9a5a63f3349082244110
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-06 19:04:31 +0100

    verify

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,7 +1,48 @@
 "use client";
 
-const SearchContent = () => {
-  return <div>Search Content</div>;
+import MediaItem from "@/components/MediaItem";
+import { Song } from "@/types";
+
+interface SearchContentProps {
+  songs: Song[];
+}
+const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+  if (songs.length === 0) {
+    return (
+      <div
+        className="
+    flex 
+    flex-col
+    gap-y-2
+    w-full
+    px-6
+    text-neutral-400
+    "
+      >
+        No Songs found!
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className="
+  flex 
+  flex-col
+  gap-y-2
+  w-full
+  px-6
+  "
+    >
+      {songs.map((song) => (
+        <div key={song.id} className="flex flex-col gap-x-4 w-full">
+          <div className="flex-1">
+            <MediaItem onClick={() => {}} data={song} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SearchContent;
