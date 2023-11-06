277685206e1467dcf87b2af4c1f937f163575a7d
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-06 16:48:49 +0100

    search input update

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,8 +1,14 @@
 "use client";
 
-import React from "react";
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const SearchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce<string>(value, 500);
+
   return <div>Search field!</div>;
 };
 
