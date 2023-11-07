239daeb735e47c6c3d3f1e88abe6e11e010faff3
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-07 21:51:19 +0100

    use effect

--- components/LikedButton.tsx
+++ components/LikedButton.tsx
@@ -1,6 +1,29 @@
 "use client";
+import { useEffect, useState } from "react";
 
-const LikedButton = () => {
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import { useSessionContext } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+
+interface LikedButtonProps {
+  songId: string;
+}
+
+const LikedButton: React.FC<LikedButtonProps> = ({ songId }) => {
+  const router = useRouter();
+  const { supabaseClient } = useSessionContext();
+
+  const authModal = useAuthModal();
+  const { user } = useUser();
+  const [isLiked, setIsLiked] = useState(false);
+
+  useEffect(() => {
+    if (!user?.id) {
+      return;
+    }
+  });
+
   return <div>Liked Button</div>;
 };
 
