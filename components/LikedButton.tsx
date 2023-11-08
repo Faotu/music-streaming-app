3d13c6eea9580c758a829123e96168d2d6d5c528
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-08 16:13:07 +0100

    trigger liked action to databas

--- components/LikedButton.tsx
+++ components/LikedButton.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
+import toast from "react-hot-toast";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { useSessionContext } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/navigation";
@@ -41,10 +42,38 @@ const LikedButton: React.FC<LikedButtonProps> = ({ songId }) => {
 
   const Icon = isLiked ? AiFillHeart : AiOutlineHeart;
 
-  const handleLike = () => {
+  const handleLike = async () => {
     if (!user) {
       return authModal.onOpen();
     }
+
+    if (isLiked) {
+      const { error } = await supabaseClient
+        .from("liked_songs")
+        .delete()
+        .eq("user_id", user.id)
+        .eq("song_id", songId);
+
+      if (error) {
+        toast.error(error.message);
+      } else {
+        setIsLiked(false);
+      }
+    } else {
+      const { error } = await supabaseClient.from("liked_songs").insert({
+        song_id: songId,
+        user_id: user.id,
+      });
+
+      if (error) {
+        toast.error(error.message);
+      } else {
+        setIsLiked(true);
+        toast.success("Liked!");
+      }
+    }
+
+    router.refresh();
   };
 
   return (
