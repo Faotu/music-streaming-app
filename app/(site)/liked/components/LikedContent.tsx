8b4337dc6eb90ca82d4fb6110bb3f753c56e144f
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-09 04:55:15 +0100

    liked unfinished and refactor

--- app/(site)/liked/components/LikedContent.tsx
+++ app/(site)/liked/components/LikedContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import MediaItem from "@/components/MediaItem";
 import { useUser } from "@/hooks/useUser";
 import { Song } from "@/types";
 import { useRouter } from "next/navigation";
@@ -25,7 +26,17 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
       </div>
     );
   }
-  return <div>Liked content</div>;
+  return (
+    <div className="flex flex-col gap-y-2 w-full p-6">
+      {songs.map((song) => (
+        <div key={song.id} className="flex items-center gap-x-4 w-full ">
+          <div className="flex-1">
+            <MediaItem onClick={() => {}} data={song} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default LikedContent;
