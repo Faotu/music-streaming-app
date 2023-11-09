582176ebf7af332749a1194db4cbaccdb87f759c
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-09 04:33:44 +0100

    No liked songs action

--- app/(site)/liked/components/LikedContent.tsx
+++ app/(site)/liked/components/LikedContent.tsx
@@ -1,6 +1,30 @@
 "use client";
 
-const LikedContent = () => {
+import { useUser } from "@/hooks/useUser";
+import { Song } from "@/types";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+interface LikedContentProps {
+  songs: Song[];
+}
+const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, user, router]);
+
+  if (songs.length === 0) {
+    return (
+      <div className="flex flex-col gap-y-4 w-full px-6 text-neutral-400">
+        No Liked Songs!
+      </div>
+    );
+  }
   return <div>Liked content</div>;
 };
 
