7510ca435a7c89ccfa1ec1d847a88fb109409563
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-10 22:12:14 +0100

    loading action

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,20 +1,20 @@
 "use client";
 
 import usePlayer from "@/hooks/usePlayer";
-// import useLoadSongUrl from "@/hooks/useLoadSongUrl";
-// import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
+import useGetSongById from "@/hooks/useGetSongById";
 
 // import PlayerContent from "./PlayerContent";
 
 const Player = () => {
-  //   const player = usePlayer();
-  //   const { song } = useGetSongById(player.activeId);
+  const player = usePlayer();
+  const { song } = useGetSongById(player.activeId);
 
-  //   const songUrl = useLoadSongUrl(song!);
+  const songUrl = useLoadSongUrl(song!);
 
-  //   if (!song || !songUrl || !player.activeId) {
-  //     return null;
-  //   }
+  if (!song || !songUrl || !player.activeId) {
+    return null;
+  }
 
   return (
     <div
