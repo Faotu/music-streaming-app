233aca534c84c231b98016439f5c1c14bf58a351
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-10 22:36:57 +0100

    player div

--- components/Player.tsx
+++ components/Player.tsx
@@ -4,7 +4,7 @@ import usePlayer from "@/hooks/usePlayer";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import useGetSongById from "@/hooks/useGetSongById";
 
-// import PlayerContent from "./PlayerContent";
+import PlayerContent from "./PlayerContent";
 
 const Player = () => {
   const player = usePlayer();
@@ -28,7 +28,7 @@ const Player = () => {
         px-4
       "
     >
-      {/* <PlayerContent key={songUrl} song={song} songUrl={songUrl} /> */}
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
       Player
     </div>
   );
