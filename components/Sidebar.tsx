45685eee890b0b7cc3f6383f9dcd8b02c6973113
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2023-11-12 23:42:29 +0100

    sidebar play song

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,6 +8,8 @@ import Box from "./Box";
 import SidebarItem from "./SidebarItem";
 import Library from "./Library";
 import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
+import { twMerge } from "tailwind-merge";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -15,6 +17,7 @@ interface SidebarProps {
 }
 const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
   const pathname = usePathname();
+  const player = usePlayer();
   const routes = useMemo(
     () => [
       {
@@ -34,7 +37,15 @@ const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
   );
 
   return (
-    <div className="flex h-full">
+    <div
+      className={twMerge(
+        `
+      flex
+      h-full
+    `,
+        player.activeId && "h-[calc(100%-80%)]"
+      )}
+    >
       <div
         className="
     hidden 
